0b6cd8428173f275a5e4ffbbf36a9d73a1e76e34
kragiea <alex.kragie@yale.edu>
2015-10-09 23:47:30 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.send('Hello MGT-656!');
+    response.send('Hello MGT-656! I am a complete ninja now');
+});
+
+app.get('/netid', function(request, response){
+    response.send('AWK25');
+});
+
+app.get('/nickname', function(request, response){
+    response.send('perfect-rabbit');
 });
 
 app.listen(process.env.PORT || 4000);
